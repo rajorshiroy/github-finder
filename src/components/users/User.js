437e203186d9259add37b6ca8461ec7bef4b634e
Rajorshi <rajorshi.me@gmail.com>
2020-08-11 03:07:49 +0530

    added gh-pages support

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,5 +1,6 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import Spinner from "../layout/Spinner";
+import {Link} from "react-router-dom";
 
 class User extends Component {
     componentDidMount() {
@@ -11,20 +12,33 @@ class User extends Component {
     render() {
         const {
             name, avatar_url, location, bio, blog, login, html_url, followers,
-            following, public_repos, public_gists, hirable
+            following, public_repos, public_gists, hireable
         } = this.props.user;
 
-        if (this.props.loading)
-            return <Spinner/>
-        else {
-
-            return (
-                <div>
-                    {name}
+        if (this.props.loading) return <Spinner/>
+        else return <Fragment>
+            <Link to='/' className='btn btn-light'>
+                <i className="fa fa-long-arrow-left"/>
+            </Link>
+            <div>
+                Hireable: {' '}
+                <i className={hireable ? 'fas fa-check text-success' : 'fas fa-times-circle text-danger'}/>
+            </div>
+            <div className="card grid-2">
+                <div className="all-center">
+                    <img src={avatar_url} alt="Avatar" className='round-img' style={{width: '150px'}}/>
+                    <h1>{name}</h1>
+                    <p>Location: {location}</p>
                 </div>
-            );
-        }
-
+                <div>
+                    {bio && <Fragment>
+                        <h3>Bio</h3>
+                        <p>{bio}</p>
+                    </Fragment>}
+                    <a href={html_url} target='_blank' className='btn btn-dark m-1'>Visit Github Profile</a>
+                </div>
+            </div>
+        </Fragment>;
     }
 }
 
