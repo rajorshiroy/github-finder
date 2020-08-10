17cdc6029a8a5f02e3f9f23550d41b6bf5696983
Rajorshi Roy <rajorshi.me@gmail.com>
2020-08-11 03:51:50 +0530

    Updates

---
+++ precache-manifest.0398da329f9c8f8c3fdf2856551907ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7634b9d26697489662189c2b92ac4a6",
+    "url": "/github-finder/index.html"
+  },
+  {
+    "revision": "c5731554f4eb4921a352",
+    "url": "/github-finder/static/css/main.33812a21.chunk.css"
+  },
+  {
+    "revision": "9caa295ad38a4f9f81b8",
+    "url": "/github-finder/static/js/2.22956abe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/github-finder/static/js/2.22956abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5731554f4eb4921a352",
+    "url": "/github-finder/static/js/main.23f4f53b.chunk.js"
+  },
+  {
+    "revision": "189fbc883acbcb16644e",
+    "url": "/github-finder/static/js/runtime-main.c7dda347.js"
+  },
+  {
+    "revision": "e464cf627229dad1d706377270bfbf1d",
+    "url": "/github-finder/static/media/spinner.e464cf62.gif"
+  }
+]);
